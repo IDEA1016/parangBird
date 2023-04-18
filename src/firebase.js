48593eaf4f8fe65b0dfbfb2deeafae335c39a1a4
상괭이 <edea1016@sang-gwaeng-is-MacBook-Air.local>
2023-04-18 14:07:54 +0900

    env파일 생성, 환경변수 세팅

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnuxBOb44vPqwZKrBessuyDe1jVY_ycRY",
-  authDomain: "parangbird-fed08.firebaseapp.com",
-  projectId: "parangbird-fed08",
-  storageBucket: "parangbird-fed08.appspot.com",
-  messagingSenderId: "164033180758",
-  appId: "1:164033180758:web:838950e945a7f309f377f7",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export const firebase = initializeApp(firebaseConfig);
