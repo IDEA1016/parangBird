564b464347f3bfbded237828d2589f0575ba403a
상괭이 <edeaswk@gmail.com>
2023-04-20 11:44:20 +0900

    회원가입, 로그인 기능 추가

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
+// firebase 환경변수 설정
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -10,5 +15,14 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
+// firebase 초기화
 const firebaseApp = initializeApp(firebaseConfig);
 export const authService = getAuth(firebaseApp);
+
+// firebase 인증 관련 함수
+export const _createUserWithEmailAndPassword = ({ email, password }) => {
+  return createUserWithEmailAndPassword(authService, email, password);
+};
+export const _signInWithEmailAndPassword = ({ email, password }) => {
+  return signInWithEmailAndPassword(authService, email, password);
+};
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,73 @@
-import React from "react";
+import {
+  _createUserWithEmailAndPassword,
+  _signInWithEmailAndPassword,
+} from "fbase";
+import React, { useEffect, useState } from "react";
 
 const Auth = () => {
-  return <span>Auth</span>;
+  const [loginForm, setLoginForm] = useState({ email: "", password: "" });
+  const [newAccount, setNewAccount] = useState(false);
+
+  const onChange = ({ target: { name, value } }) => {
+    setLoginForm({ ...loginForm, [name]: value });
+  };
+
+  useEffect(() => {
+    console.log(loginForm);
+  }, [loginForm]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        //create account
+        data = await _createUserWithEmailAndPassword({
+          email: loginForm.email,
+          password: loginForm.password,
+        });
+      } else {
+        data = await _signInWithEmailAndPassword({
+          email: loginForm.email,
+          password: loginForm.password,
+        });
+      }
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={loginForm.email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={loginForm.password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create New Account" : "Log in"}
+        />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
